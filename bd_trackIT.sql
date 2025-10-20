12ce3deeacb698d413e06d5711c0e25d2e8896ed
geohpassos <110428388+geohpassos@users.noreply.github.com>
2025-10-20 20:23:21 -0300

    Banco de Dados

---
+++ bd_trackIT.sql
@@ -0,0 +1,43 @@
+CREATE DATABASE trackIt;
+USE trackIt;
+CREATE TABLE login(
+idLogin INT AUTO_INCREMENT PRIMARY KEY,
+email VARCHAR(250),
+senha VARCHAR(250)
+);
+CREATE TABLE administrador(
+idAdm INT AUTO_INCREMENT PRIMARY KEY,
+nomeCompleto VARCHAR(250),
+email VARCHAR(250),
+senha VARCHAR(250),
+idlogin INT, 
+FOREIGN KEY (idlogin) REFERENCES login(idLogin)
+);
+CREATE TABLE proprietario(
+idUsuario INT AUTO_INCREMENT PRIMARY KEY,
+nomeCompleto VARCHAR(250),
+email VARCHAR(250),
+senha VARCHAR(250),
+idlogin INT, 
+FOREIGN KEY (idlogin) REFERENCES login(idLogin)
+);
+CREATE TABLE arduino(
+codArduino VARCHAR(30) PRIMARY KEY
+);
+CREATE TABLE veiculos(
+idVeiculos INT AUTO_INCREMENT PRIMARY KEY,
+placa VARCHAR(7),
+modelo VARCHAR(250),
+idProprietario INT,
+codArduino VARCHAR(30),
+FOREIGN KEY (idProprietario) REFERENCES proprietario(idUsuario),
+FOREIGN KEY (codArduino) REFERENCES arduino(codArduino)
+);
+CREATE TABLE localizacao(
+idLocalizacao INT AUTO_INCREMENT PRIMARY KEY,
+token_dispositivo VARCHAR(50) NOT NULL,
+latitude DECIMAL(10,6) NOT NULL,
+longitude DECIMAL(10,6) NOT NULL,
+altitude DECIMAL(10,2),
+timestamp_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
